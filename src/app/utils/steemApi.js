f9a666ad35e110d5026ef1b326212b1ae4b8a7d4
Evan <choue@google.com>
2019-10-02 02:54:41 +0000

    to make old steemkeychain config work

--- src/app/utils/steemApi.js
+++ src/app/utils/steemApi.js
@@ -6,8 +6,14 @@ export async function getStateAsync(url) {
     // strip off query string
     const path = url.split('?')[0];
 
-    const raw = await api.getStateAsync(path);
+    let raw = await api.getStateAsync(path);
 
+    if (!raw.accounts) {
+        raw.accounts = {};
+    }
+    if (!raw.content) {
+        raw.content = {};
+    }
     const cleansed = stateCleaner(raw);
 
     return cleansed;
